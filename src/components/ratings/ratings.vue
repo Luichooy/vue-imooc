8ad2ceda37bf8192d76daac7a864d0cc94eae9fb
luichooy <luichooy@163.com>
2017-09-18 11:04:49 +0800

    评价路由中的滚动调整

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,67 +1,71 @@
 <template>
   <div class="ratings">
-      <div class="ratings-top clearfix">
-        <div class="left border-width">
-          <p class="mark">{{seller.score}}</p>
-          <p class="mark-type">综合评分</p>
-          <p class="mark-desc">高于周边商家{{seller.rankRate}}%</p>
-        </div>
-        <div class="right">
-          <div class="score-wrapper">
-            <span class="label">服务态度</span>
-            <div class="star-wrapper">
-              <star :size="24" :score="seller.serviceScore"></star>
-            </div>
-            <span class="score">{{seller.serviceScore}}</span>
+    <div class="ratings-wrapper" ref="ratings-wrapper">
+      <div class="ratings-content">
+        <div class="ratings-top clearfix">
+          <div class="left border-width">
+            <p class="mark">{{seller.score}}</p>
+            <p class="mark-type">综合评分</p>
+            <p class="mark-desc">高于周边商家{{seller.rankRate}}%</p>
           </div>
-          <div class="score-wrapper">
-            <span class="label">商品评分</span>
-            <div class="star-wrapper">
-              <star :size="24" :score="seller.foodScore"></star>
-            </div>
-            <span class="score">{{seller.foodScore}}</span>
-          </div>
-          <div class="score-wrapper">
-            <span class="label">送达时间</span>
-            <span class="delivery-time">{{seller.deliveryTime}}</span>
-          </div>
-        </div>
-      </div>
-      <split></split>
-      <div class="ratings-wrapper">
-        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
-                      :ratings="ratings" v-on:toggleType="toggleType"
-                      v-on:toggleOnlyContent="toggleOnlyContent"></ratingselect>
-        <div class="ratings-list" ref="ratings-list">
-          <ul>
-            <li class="rating-item border-height" v-for="rating in ratings">
-              <div class="avatar-wrapper">
-                <img :src="rating.avatar" alt="" width="100%" height="100%">
+          <div class="right">
+            <div class="score-wrapper">
+              <span class="label">服务态度</span>
+              <div class="star-wrapper">
+                <star :size="24" :score="seller.serviceScore"></star>
               </div>
-              <div class="rating-detail">
-                <div class="rating-user clearfix">
-                  <span class="username">{{rating.username}}</span>
-                  <span class="rating-time">{{rating.rateTime}}</span>
+              <span class="score">{{seller.serviceScore}}</span>
+            </div>
+            <div class="score-wrapper">
+              <span class="label">商品评分</span>
+              <div class="star-wrapper">
+                <star :size="24" :score="seller.foodScore"></star>
+              </div>
+              <span class="score">{{seller.foodScore}}</span>
+            </div>
+            <div class="score-wrapper">
+              <span class="label">送达时间</span>
+              <span class="delivery-time">{{seller.deliveryTime}}</span>
+            </div>
+          </div>
+        </div>
+        <split></split>
+        <div class="ratings-list-wrapper">
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                        :ratings="ratings" v-on:toggleType="toggleType"
+                        v-on:toggleOnlyContent="toggleOnlyContent"></ratingselect>
+          <div class="ratings-list">
+            <ul>
+              <li class="rating-item border-height" v-for="rating in ratings">
+                <div class="avatar-wrapper">
+                  <img :src="rating.avatar" alt="" width="100%" height="100%">
                 </div>
-                <div class="rating-star">
-                  <div class="star-wrapper">
-                    <star :size="36" :score="rating.score"></star>
+                <div class="rating-detail">
+                  <div class="rating-user clearfix">
+                    <span class="username">{{rating.username}}</span>
+                    <span class="rating-time">{{rating.rateTime}}</span>
                   </div>
-                  <span class="dilivery-time">{{rating.deliveryTime}}分钟送达</span>
+                  <div class="rating-star">
+                    <div class="star-wrapper">
+                      <star :size="36" :score="rating.score"></star>
+                    </div>
+                    <span class="dilivery-time">{{rating.deliveryTime}}分钟送达</span>
+                  </div>
+                  <div class="rating-text">{{rating.text}}</div>
+                  <div class="recommend-wrapper">
+                    <i class="rating-icon"
+                       :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></i>
+                    <ul class="recommend-list">
+                      <li class="recommend-item" v-for="recommend in rating.recommend">{{recommend}}</li>
+                    </ul>
+                  </div>
                 </div>
-                <div class="rating-text">{{rating.text}}</div>
-                <div class="recommend-wrapper">
-                  <i class="rating-icon"
-                     :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></i>
-                  <ul class="recommend-list">
-                    <li class="recommend-item" v-for="recommend in rating.recommend">{{recommend}}</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+    </div>
     <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice"
               :min-price="seller.minPrice"></shopcart>
   </div>
@@ -105,12 +109,12 @@
           this.ratings = res.data;
 
           this.$nextTick(function () {
-            if (!this.ratingList) {
-              this.ratingList = new BScroll(this.$refs['ratings-list'], {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$refs['ratings-wrapper'], {
                 click: true
               });
             } else {
-              this.ratingList.refresh();
+              this.scroll.refresh();
             }
           });
         } else {
@@ -141,125 +145,126 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .ratings
-    .ratings-top
-      padding: 18px 0
-      .left
-        width: 138px
-        float: left
-        border-right(rgba(7, 17, 27, 0.1))
-        box-sizing: border-box
-        text-align: center
-        .mark
-          line-height: 28px
-          font-size: 24px
-          color: rgb(255, 153, 0)
-        .mark-type
-          margin: 6px 0 8px 0
-          line-height: 12px
-          font-size: 12px
-          color: rgb(7, 17, 27)
-        .mark-desc
-          margin-bottom: 6px
-          line-height: 10px
-          font-size: 10px
-          color: rgba(7, 17, 27, 0.5)
-      .right
-        float: left
-        padding: 0 24px
-        & > .score-wrapper + .score-wrapper
-          margin-top: 8px
-        .score-wrapper
-          .label
-            margin-right: 12px
-            line-height 18px
+    .ratings-wrapper
+      position : fixed
+      top: 174px
+      bottom: 48px
+      left: 0
+      right: 0
+      overflow: hidden
+      .ratings-top
+        padding: 18px 0
+        .left
+          width: 138px
+          float: left
+          border-right(rgba(7, 17, 27, 0.1))
+          box-sizing: border-box
+          text-align: center
+          .mark
+            line-height: 28px
+            font-size: 24px
+            color: rgb(255, 153, 0)
+          .mark-type
+            margin: 6px 0 8px 0
+            line-height: 12px
             font-size: 12px
             color: rgb(7, 17, 27)
-          .star-wrapper
-            display: inline-block
-            margin-right: 12px
-          .score
-            line-height: 18px
-            font-size: 12px
-            color: rgb(255, 153, 0)
-          .delivery-time
-            line-height: 18px
-            font-size: 12px
-            color: rgb(147, 153, 159)
-
-    .ratings-wrapper
-      .ratings-list
-        position : fixed
-        top: 414px
-        bottom: 48px
-        left: 0
-        right: 0
-        overflow: hidden
-        padding: 0 18px
-        .rating-item + .rating-item
-          border-top(rgba(7, 17, 27, 0.1))
-        .rating-item
-          display: flex
-          padding: 18px 0
-        .avatar-wrapper
-          flex: 0 0 28px
-          padding-right: 12px
-          width: 28px
-          height: 28px
-          img
-            border-radius: 50%
-        .rating-detail
-          flex: 1 1 auto
-          /*vertical-align: top*/
-          .rating-user
-            margin-bottom: 4px
-            .username
-              float: left
-              line-height: 12px
-              font-size: 10px
+          .mark-desc
+            margin-bottom: 6px
+            line-height: 10px
+            font-size: 10px
+            color: rgba(7, 17, 27, 0.5)
+        .right
+          float: left
+          padding: 0 24px
+          & > .score-wrapper + .score-wrapper
+            margin-top: 8px
+          .score-wrapper
+            .label
+              margin-right: 12px
+              line-height 18px
+              font-size: 12px
               color: rgb(7, 17, 27)
-            .rating-time
-              float: right
-              line-height: 12px
-              font-size: 10px
-              font-weight: 200
-              color: rgb(147, 153, 159)
-          .rating-star
-            font-size: 0
             .star-wrapper
               display: inline-block
-              vertical-align: middle
               margin-right: 12px
-            .dilivery-time
-              display: inline-block
-              vertical-align: middle
-              line-height: 12px
-              font-size: 10px
-              font-weight: 200
-              color: rgb(147, 153, 159)
-          .rating-text
-            margin: 6px 0 8px 0
-            line-height: 18px
-            font-size: 12px
-            color: rgb(7, 17, 27)
-          .recommend-wrapper
-            .rating-icon
-              display: inline-block
-              margin-right: 8px
-              line-height: 16px
+            .score
+              line-height: 18px
               font-size: 12px
-              &.icon-thumb_up
-                color: rgb(0, 160, 220)
-              &.icon-thumb_down
-                color: rgb(183, 187, 191)
-            .recommend-list
-              display: inline-block
+              color: rgb(255, 153, 0)
+            .delivery-time
+              line-height: 18px
+              font-size: 12px
+              color: rgb(147, 153, 159)
+
+      .ratings-list-wrapper
+        .ratings-list
+          padding: 0 18px
+          .rating-item + .rating-item
+            border-top(rgba(7, 17, 27, 0.1))
+          .rating-item
+            display: flex
+            padding: 18px 0
+          .avatar-wrapper
+            flex: 0 0 28px
+            padding-right: 12px
+            width: 28px
+            height: 28px
+            img
+              border-radius: 50%
+          .rating-detail
+            flex: 1 1 auto
+            /*vertical-align: top*/
+            .rating-user
+              margin-bottom: 4px
+              .username
+                float: left
+                line-height: 12px
+                font-size: 10px
+                color: rgb(7, 17, 27)
+              .rating-time
+                float: right
+                line-height: 12px
+                font-size: 10px
+                font-weight: 200
+                color: rgb(147, 153, 159)
+            .rating-star
               font-size: 0
-              .recommend-item
+              .star-wrapper
+                display: inline-block
+                vertical-align: middle
+                margin-right: 12px
+              .dilivery-time
+                display: inline-block
+                vertical-align: middle
+                line-height: 12px
+                font-size: 10px
+                font-weight: 200
+                color: rgb(147, 153, 159)
+            .rating-text
+              margin: 6px 0 8px 0
+              line-height: 18px
+              font-size: 12px
+              color: rgb(7, 17, 27)
+            .recommend-wrapper
+              .rating-icon
                 display: inline-block
                 margin-right: 8px
-                padding: 0 6px
                 line-height: 16px
-                border: 1px solid rgba(7, 17, 27, 0.1)
-                font-size: 9px
-                color: rgb(147, 153, 159)
+                font-size: 12px
+                &.icon-thumb_up
+                  color: rgb(0, 160, 220)
+                &.icon-thumb_down
+                  color: rgb(183, 187, 191)
+              .recommend-list
+                display: inline-block
+                font-size: 0
+                .recommend-item
+                  display: inline-block
+                  margin-right: 8px
+                  padding: 0 6px
+                  line-height: 16px
+                  border: 1px solid rgba(7, 17, 27, 0.1)
+                  font-size: 9px
+                  color: rgb(147, 153, 159)
 </style>
